39d21aa834832ddf6f189556b913f618bedd022c
Miruna <mariamiruna2015@gmail.com>
2023-10-10 12:44:24 +0300

    october

---
+++ october.py
@@ -0,0 +1,15 @@
+if x%2 == 0:
+    print("it's even")
+
+for i in range(1,5):
+    if i % 3 == 0:
+        print(i+4)
+    print(i)
+
+s: int =0
+while x > 0:
+    c: int = x % 10
+    s = s + c
+    x = x / 10
+    
+
